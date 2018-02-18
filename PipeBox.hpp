aee58588ad1312b45f7ac524d68ce26c71676178
yurychu <yury_chugunov@mail.ru>
2018-02-19 00:35:31 +0300

    pipebox empty

---
+++ PipeBox.hpp
@@ -0,0 +1,21 @@
+#ifndef CIRCULAR_BALANCING_BUFFER_PIPEBOX_HPP
+#define CIRCULAR_BALANCING_BUFFER_PIPEBOX_HPP
+
+namespace util_lib {
+
+    template <typename OutputT, typename InputT, typename FunctorT>
+    class PipeBox
+    {
+    private:
+        FunctorT itsFunctor;
+
+    public:
+        PipeBox();
+        ~PipeBox();
+
+
+    };
+
+
+}
+#endif  // CIRCULAR_BALANCING_BUFFER_PIPEBOX_HPP
