973d5eb6f4188d4db2430ccae4cd459b3ae26334
yurychu <yury_chugunov@mail.ru>
2018-10-12 17:28:25 +0300

    simple table 2

--- TableOne.hpp
+++ TableOne.hpp
@@ -2,26 +2,10 @@
 #define UTILS_TABLEONE_HPP
 
 #include <initializer_list>
+#include <map>
 #include <string>
 
 namespace to {
-
-
-    class Cell
-    {
-    private:
-        u_char itsTypeId;
-
-    public:
-
-        Cell()
-        {
-        }
-
-        ~Cell() = default;
-
-
-    };
 
     using ValueType = int64_t;
 
@@ -47,42 +31,85 @@ namespace to {
 
     };
 
-//
-//    class HeadCell
-//    {
-//    private:
-//        static size_t inst_counter;
-//        size_t itsIdx;
-//
-//    public:
-//        HeadCell()
-//                : itsIdx(++inst_counter)
-//        {
-//        }
-//
-//
-//    };
 
-//    HeadCell::inst_counter = 0;
+    class HeadCell
+    {
+    private:
+        static size_t inst_counter;
+        size_t itsIdx;
+
+    public:
+        HeadCell()
+                : itsIdx(++inst_counter)
+        {
+        }
+
+        size_t get_idx() const
+        {
+            return itsIdx;
+        }
+
+
+    };
+
+    size_t HeadCell::inst_counter = 0;
+
+
+    class Cell
+    {
+    private:
+        size_t itsCellIdx;
+        ValueType itsValue;
+
+    public:
+
+        Cell(size_t cell_idx, ValueType value)
+            : itsCellIdx(cell_idx),
+              itsValue(value)
+        {
+        }
+
+        ~Cell() = default;
+
+        size_t get_cell_idx() const
+        {
+            return itsCellIdx;
+        }
+
+        ValueType get_value() const
+        {
+            return itsValue;
+        }
+
+
+    };
+
 
     class Table
     {
     private:
+        size_t itsInserts;
+        std::map<std::string, HeadCell> itsHeads;
 
     public:
 
         Table()
+            : itsHeads(),
+              itsInserts(0)
         {
-
         }
 
         ~Table() = default;
 
         void insert(const std::initializer_list<InjectElement> & l)
         {
+            const auto cur_ins = ++itsInserts;
             for (const auto & el : l){
                 auto name = el.get_name();
                 auto value = el.get_value();
+
+                auto cell_head = itsHeads[name];
+                std::cout << cell_head.get_idx()<< std::endl;
             }
         }
 
--- main.cpp
+++ main.cpp
@@ -1,18 +1,43 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <list>
+#include <chrono>
+#include <thread>
 
 #include "TableOne.hpp"
 
+class A
+{
+private:
+    int64_t a;
+    int64_t b;
+    int64_t c;
+    int64_t d;
+    int64_t e;
+};
+
+class B
+{
+private:
+    A a;
+    A b;
+    A c;
+    A d;
+    A e;
+};
 
 int main(int argc, char * argv[])
 {
+    std::cout << "Begin" << std::endl;
+    std::list<B> lst;
 
-//    to::Table table;
-//
-//    table.insert({{"abc", 40}, {"xyz", 60}});
+    for (int i = 0; i < 2000000; ++i){
+        lst.push_back({});
 
-    bool a = true;
-    std::cout << sizeof(a) << std::endl;
+//        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
 
     return EXIT_SUCCESS;
 }
